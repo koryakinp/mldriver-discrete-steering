56f881edd6dae6e91a3d45e92295e94399958fa6
koryakinp <koryakinp@koryakinp.com>
2019-05-05 02:54:39 -0400

    -minor fix

--- consts.py
+++ consts.py
@@ -2,5 +2,5 @@ OBS_SIZE = 128
 GAMMA = 0.95
 LR = 0.001
 BASELINE_PERIOD = 10000
-ENV_NAME = "/mldriver-discrete-steering/mldriver-discrete-steering.app"
+ENV_NAME = "./mldriver-discrete-steering.app"
 CHECKPOINT_FILE = "./checkpoints/mldriver.ckpt"
