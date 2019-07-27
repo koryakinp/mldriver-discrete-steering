d9a1e9bc0a8269e688866c3aa41032d6352afa4a
koryakinp <koryakinp@koryakinp.com>
2019-07-27 13:24:44 -0400

    -updated runner.sh

--- runner.sh
+++ runner.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
-while getopts ":c:" opt; do
-  case $opt in
-    c)
-      mv output/config/$OPTARG consts.py
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit 1
-      ;;
-    :)
-      echo "Option -$OPTARG requires an argument." >&2
-      exit 1
-      ;;
-  esac
-done
-
-while getopts ":e:" opt; do
+while getopts "ce" opt; do
   case $opt in
     e)
       xvfb-run -a -s "-screen 0 128x128x24" -- python main.py $OPTARG
+      ;;
+    c)
+      mv output/config/$OPTARG consts.py
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
