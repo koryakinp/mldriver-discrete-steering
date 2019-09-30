1a1725613db7e88ddbd78f0b54a7554e2c460d2f
koryakinp <koryakinp@koryakinp.com>
2019-09-29 23:00:40 -0400

    -minor fixes

--- runner.sh
+++ runner.sh
@@ -4,8 +4,7 @@ cd /python-env/mldriver-discrete-steering
 while getopts ":c:e:" opt; do
   case $opt in
     e)
-      pipenv shell
-      xvfb-run -a -s "-screen 0 128x128x24" -- python main.py $OPTARG
+      pipenv run xvfb-run -a -s "-screen 0 128x128x24" -- python main.py $OPTARG
       ;;
     c)
       cp "output/config/$OPTARG" consts.py
